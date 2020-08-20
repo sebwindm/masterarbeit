c392d10f768f7e47b59b925d44a94ed0f3c8db7b
Sebastian Windmueller <sebastian.windmueller@gmail.com>
2020-08-20 14:53:55 +0200

    fixed a potential TypeError

--- ReinforcementLearning/util.py
+++ ReinforcementLearning/util.py
@@ -4,4 +4,4 @@ def exponential_decay(min_value: float, rate: float, steps: int, period: int, va
     """
     Return the exponentially decayed value: max(minVal, rate ^ (period / steps) * value)
     """
-    return max(min_value, rate ^ (period / steps) * value)
+    return max(min_value, rate ** (period / steps) * value)
