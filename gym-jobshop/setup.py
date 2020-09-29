35ead224bd473edb9d1ee31ad3af7fac96fc0974
Sebastian Windmueller <sebastian.windmueller@gmail.com>
2020-09-29 18:35:42 +0200

    Add numpy as a dependency

--- gym-jobshop/setup.py
+++ gym-jobshop/setup.py
@@ -1,6 +1,7 @@
 from setuptools import setup
 
 setup(name='gym_jobshop',
-      version='0.0.1',
-      install_requires=['gym']#And any other dependencies required
+      version='1.0.0',
+      install_requires=['gym',
+                        'numpy']#And any other dependencies required
 )
